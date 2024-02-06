f7f42487aefe9fcb776592a6cf0c58393f1d1681
Max Berman <mlazerb@gmail.com>
2024-02-06 15:54:17 +0100

    Added test file for the Arduino UNO to be the SolTag Mimic

---
+++ SolTagMimic/SolTagMimic.ino
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+void setup() {
+  // set the digital pin as output:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin, ledState);
+  }
+}
