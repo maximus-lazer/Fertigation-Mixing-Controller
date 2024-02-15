336d633adae69e68efbfbc1be6c277b6cc0f74e6
Max Berman <mlazerb@gmail.com>
2024-02-15 12:14:15 +0100

    Tested running the L298N motor driver to actuate the solenoids

--- SolenoidTest/SolenoidTest.ino
+++ SolenoidTest/SolenoidTest.ino
@@ -1,35 +1,33 @@
-#include <AFMotor.h>                //you must download and install this library: https://drive.google.com/file/d/1zsMywqJjvzgMBoVZyrYly-2hXePFXFzw/view?usp=sharing
+#define in1 A4
+#define in2 A5
+const int pulse = 100;
+const int wait = 30000;
 
-
-
-//These lines declare which ports your motors will be connected to on the motor shield.
-AF_DCMotor solenoid(2);
-
-
-// In setup, we tell bluetooth communication to start and set all of our motors to not move
 void setup() {
   Serial.begin(9600);
-  solenoid.setSpeed(255);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
 }
 
 void loop() {
   Serial.println("FORWARD");
-  solenoid.run(FORWARD);     // These comands tell the motors what speed and direction to move
-  // solenoid.setSpeed(255);
-  delay(2000);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  delay(pulse);
   Serial.println("STOP");
-  solenoid.run(RELEASE);
-  // solenoid.setSpeed(0);
-  delay(5000);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  delay(wait);
   Serial.println("BACKWARD");
-  solenoid.run(BACKWARD);     // These comands tell the motors what speed and direction to move
-  // solenoid.setSpeed(255);
-  delay(2000);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  delay(pulse);
   Serial.println("STOP");
-  solenoid.run(RELEASE);
-  // solenoid.setSpeed(0);
-  delay(5000);
-  
-
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  delay(wait);
 }
 
